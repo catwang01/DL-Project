9eea9b9afdaad1d1af5843e133e0411708bff362
catwang01 <2015201620@ruc.edu.cn>
2020-06-18 00:33:15 +0800

    finish fm-tf2-with-embedding-lookup.py with has only one sparse features

--- Recommender/FM/fm-tf2-with-embedding-lookup.py
+++ Recommender/FM/fm-tf2-with-embedding-lookup.py
@@ -11,24 +11,33 @@ n = n_sparse_features + n_dense_features # feature size
 k = 3  # embedding size
 
 np.random.seed(123)
-x_dense = np.random.normal(size=(m, n_dense_features))
-
-x_sparse = np.random.randint(0, 4, size=(m,1))
+x_dense_ = np.random.normal(size=(m, n_dense_features))
+x_sparse_ = np.random.randint(0, 4, size=(m,))
 encoder = OneHotEncoder(categories=[[0,1,2,3]], sparse=False)
-x_sparse_one_hot = encoder.fit_transform(x_sparse)
-x_sparse_one_hot
-x_ = np.c_[x_sparse, x_dense]
+# x_sparse_one_hot = encoder.fit_transform(x_sparse_)
+# x_sparse_one_hot
+# x_ = np.c_[x_sparse_, x_dense_]
 y_ = np.random.normal(size=(m,))
 
-x = tf.constant(x_, dtype=tf.float32)
+x_sparse = tf.constant(x_sparse_, dtype=tf.int32)
+x_dense = tf.constant(x_dense_, dtype=tf.float32)
 y = tf.constant(y_, dtype=tf.float32)
-w0 =  tf.Variable(0.0)
-w1 = tf.Variable(np.random.normal(size=(n, 1)), dtype=tf.float32)
-v = tf.Variable(np.random.normal(size=(n, k)), dtype=tf.float32)
 
-def forward(x, w0, w1, v):
-    linear = w0 +  tf.squeeze(tf.matmul(x, w1))
-    y =  linear + 1/2 * tf.reduce_mean(tf.matmul(x, v) ** 2 - tf.matmul(x ** 2, v ** 2), axis=1)
+w0 =  tf.Variable(0.0)
+w1_dense = tf.Variable(np.random.normal(size=(n_dense_features, 1)), dtype=tf.float32)
+w1_sparse = tf.Variable(np.random.normal(size=(n_class, 1)), dtype=tf.float32)
+v_sparse = tf.Variable(np.random.normal(size=(n_class, k)), dtype=tf.float32)
+v_dense = tf.Variable(np.random.normal(size=(n_dense_features, k)), dtype=tf.float32)
+
+def forward(x_dense, x_sparse, w0, w1_dense, w1_sparse, v_dense, v_sparse):
+    linear_dense = tf.squeeze(tf.matmul(x_dense, w1_dense))
+    linear_sparse = tf.squeeze(tf.nn.embedding_lookup(w1_sparse, x_sparse))
+    linear = w0 + linear_dense + linear_sparse
+
+    mul_square = (tf.nn.embedding_lookup(v_sparse, x_sparse) + tf.matmul(x_dense, v_dense)) ** 2 
+    square_mul = (tf.matmul(x_dense ** 2, v_dense**2) + tf.nn.embedding_lookup(v_sparse**2, x_sparse))
+
+    y = linear + 1/2 * tf.reduce_mean(mul_square - square_mul , axis=1)
     return y
 
 optizmier = tf.keras.optimizers.SGD(lr=0.001)
@@ -37,21 +46,15 @@ loss_list = []
 n_epochs = 1000
 for epoch in range(n_epochs):
     with tf.GradientTape() as tape:
-        yhat = forward(x, w0, w1, v)
+        yhat = forward(x_dense, x_sparse, w0, w1_dense, w1_sparse, v_dense, v_sparse)
         mse = tf.reduce_mean(tf.losses.mse(y, yhat))
-    grads = tape.gradient(mse, [w0, w1, v])
-    optizmier.apply_gradients(zip(grads, [w0, w1, v]))
+    grads = tape.gradient(mse, [w0, w1_dense, w1_sparse, v_dense, v_sparse])
+    optizmier.apply_gradients(zip(grads, [w0, w1_dense, w1_sparse, v_dense, v_sparse]))
     loss_list.append(mse.numpy())
 
-plt.ion()
 plt.plot(loss_list)
 plt.title("Loss")
-plt.pause(0.05)
-plt.close()
+plt.show()
 
 print(y)
-print(forward(x, w0, w1, v))
-
-# tf.Tensor([ 0.82711744  1.0707815  -3.0555775   1.005932   -1.7650565 ], shape=(5,), dtype=float32)
-# tf.Tensor([ 0.8262203  1.0728326 -3.0549483  1.0054502 -1.7684705], shape=(5,), dtype=float32)
-
+print(forward(x_dense, x_sparse, w0, w1_dense, w1_sparse, v_dense, v_sparse))
